e50645b32c199cdfb1e8dfc987400561883f410d
Lucas Zarzur <lucas12zarzur@gmail.com>
2020-05-24 18:44:58 -0300

    Spring configuration with Java code (no xml)

---
+++ src/com/lucas/springdemo/SportConfig.java
@@ -0,0 +1,9 @@
+package com.lucas.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lucas.springdemo")
+public class SportConfig {
+}
